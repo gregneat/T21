84dabce8fbd11f5a1751c6622c466bbeb00891bf
Tony Yeon <32084733+Runningsloths@users.noreply.github.com>
2020-07-22 19:35:51 -0700

    added 002

---
+++ 003 String class/base code/starter.java
@@ -0,0 +1,14 @@
+class starter {
+        public static void main(String args[])
+        {
+//  	This is how you make a comment in java (c++ too)
+//	neat is a reference to a String object.
+//	This line constructs a String object and sets its value to Happy.
+		String neat = new String("Happy");
+		System.out.print(neat); 
+		System.out.print(neat);
+		System.out.println(neat);
+		System.out.print(neat + "sad");
+        }
+        
+}
