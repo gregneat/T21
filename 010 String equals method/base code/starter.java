46577b3b5e0b9bf64ca171b6e5c15b0090058f29
Tony Yeon <32084733+Runningsloths@users.noreply.github.com>
2020-08-05 17:08:07 -0700

    added 010

---
+++ 010 String equals method/base code/starter.java
@@ -0,0 +1,20 @@
+import pkg.*;
+
+public class starter implements InputKeyControl {
+		
+		
+        public static void main(String args[])
+        {
+			// please leave following line alone, necessary for keyboard input
+			KeyController mC = new KeyController(Canvas.getInstance(),new starter());
+			
+			// enter code here
+			
+		}
+		public void keyPress(String s)
+		{
+			// enter code here
+			// note, s is the String representation of the key pressed
+		}
+
+}
---
+++ 010 String equals method/f010.bat
@@ -0,0 +1,2 @@
+@echo off
+start https://youtu.be/e7TR72FnLxk
