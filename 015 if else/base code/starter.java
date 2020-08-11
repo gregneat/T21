96e3496f02a840a7b40b652e41f4a3e3378e86f3
Tony Yeon <32084733+Runningsloths@users.noreply.github.com>
2020-08-10 17:31:34 -0700

    added 015

---
+++ 015 if else/base code/starter.java
@@ -0,0 +1,19 @@
+import pkg.*;
+
+public class starter implements InputKeyControl {
+		
+		
+        public static void main(String args[])
+        {
+			// please leave following line alone, necessary for keyboard input
+			KeyController mC = new KeyController(Canvas.getInstance(),new starter());
+			
+			// enter code here
+			
+		}
+		public void keyPress(String s)
+		{
+			// enter code here
+		}
+
+}
