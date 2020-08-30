506a3df7638f8e8efe3f4c66664f0aa757f1e7ff
Tony Yeon <32084733+Runningsloths@users.noreply.github.com>
2020-08-30 15:30:10 -0700

    base code cleanup

--- 000 intro/base code/starter.java
+++ 000 intro/base code/starter.java
@@ -1,10 +1,10 @@
 class starter {
-        public static void main(String args[])
-        {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		
-			System.out.print("I love to learn coding remotely."); 
-        }
+    public static void main(String args[])
+    {
+      // the string "I love to learn coding remotely." will appear in
+      // the command window when you compile and run this program.
+
+      System.out.print("I love to learn coding remotely."); 
+    }
         
 }
--- input base code/starter.java
+++ input base code/starter.java
@@ -3,11 +3,11 @@ import java.util.*;
 
 class starter {
 		
-        public static void main(String args[])
-        {
-			// put your code here
-			
-			
-        }
+    public static void main(String args[])
+    {
+      // put your code here
+
+
+    }
         
 }
