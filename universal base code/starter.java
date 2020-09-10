8c09fadd2493fac363abdab385fd5935511662be
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2020-09-10 10:15:15 -0700

    made UBC properly formatted
    removed random indents and fixed improperly aligned segments

--- universal base code/starter.java
+++ universal base code/starter.java
@@ -1,29 +1,23 @@
 import pkg.*;
 
 public class starter implements InputControl, InputKeyControl {
-		
-		
-		public static void main(String args[])
-		{
-			// please leave following line alone, necessary for keyboard/mouse input
-      KeyController kC = new KeyController(Canvas.getInstance(),new starter());
-      MouseController mC = new MouseController(Canvas.getInstance(),new starter());
 			
-			// enter code here
-		
-			
-    }
+	public static void main(String args[]) {
+		// please leave following line alone, necessary for keyboard/mouse input
+		KeyController kC = new KeyController(Canvas.getInstance(),new starter());
+		MouseController mC = new MouseController(Canvas.getInstance(),new starter());
 
-    public void onMouseClick(double x, double y)
-    {
-			// enter code here
-	
-    }
-    
-		public void keyPress(String s)
-		{
-			// enter code here
-			
-		}
+		// enter code here
 
+	}
+
+	public void onMouseClick(double x, double y) {
+		// enter code here
+
+	}
+
+	public void keyPress(String s) {
+		// enter code here
+
+	}
 }
