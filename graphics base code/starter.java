df64ae0c931d86060191f5b142839dec97e0477c
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2020-09-10 19:41:16 -0700

    made gbc pretty

--- graphics base code/starter.java
+++ graphics base code/starter.java
@@ -1,23 +1,16 @@
 import pkg.*;
 
 public class starter implements InputControl {
-		
-		
-    public static void main(String args[])
-    {
-      // please leave following line alone, necessary for mouse input
-      MouseController mC = new MouseController(Canvas.getInstance(),new starter());
-      // use "universal base code" for keyPress method
+	public static void main(String args[]) {
+		// please leave following line alone, necessary for mouse input
+		MouseController mC = new MouseController(Canvas.getInstance(),new starter());
+		// use "universal base code" for keyPress method
 
-      // enter code here
+		// enter code here
 
+    	}
 
-    }
-
-    public void onMouseClick(double x, double y)
-    {
-			// and/or here
-	
-    }
-
+	public void onMouseClick(double x, double y) {
+	    //or here
+	}
 }
