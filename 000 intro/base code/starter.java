0ddd5e0ff83529fc50ed1fa70c481f1343347203
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2020-09-10 19:47:42 -0700

    made it nicer

--- 000 intro/base code/starter.java
+++ 000 intro/base code/starter.java
@@ -1,10 +1,8 @@
 class starter {
-    public static void main(String args[])
-    {
-      // the string "I love to learn coding remotely." will appear in
-      // the command window when you compile and run this program.
+	public static void main(String args[]) {
+	  // the string "I love to learn coding remotely." will appear in
+	  // the command window when you compile and run this program.
 
-      System.out.print("I love to learn coding remotely."); 
-    }
-        
+	  System.out.print("I love to learn coding remotely."); 
+	}
 }
