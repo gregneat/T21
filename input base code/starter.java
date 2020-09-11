f2633937b148962ec155af72162ca9229b5b1757
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2020-09-10 19:43:22 -0700

    made it nicer
    fixed indents and weird linebreaks/empties

--- input base code/starter.java
+++ input base code/starter.java
@@ -1,13 +1,8 @@
-
 import java.util.*;
 
 class starter {
+	public static void main(String args[]) {
+		// put your code here
 		
-    public static void main(String args[])
-    {
-      // put your code here
-
-
-    }
-        
+	}   
 }
